5862ad09b69aa34768dc55576077c741ecf09aea
Mihri <mihribanguneydas@gmail.com>
2022-10-24 17:18:04 -0500

    Update and rename missileData.sql to missile_data.sql

--- Assignments/P04/missile_data.sql
+++ Assignments/P04/missile_data.sql
@@ -1,22 +1,24 @@
--- Table: public.missileData
+-- Table: public.missile_data
 
--- DROP TABLE IF EXISTS public."missileData";
+-- DROP TABLE IF EXISTS public.missile_data;
 
-CREATE TABLE IF NOT EXISTS public."missileData"
+CREATE TABLE IF NOT EXISTS public.missile_data
 (
     missile_id real NOT NULL,
     "current_time" time without time zone NOT NULL,
     current_loc geometry NOT NULL,
-    target_id numeric NOT NULL,
+    target_id real NOT NULL,
     target_city geometry NOT NULL,
     start_time time without time zone NOT NULL,
     start_loc geometry NOT NULL,
     speed real NOT NULL,
     altitude real NOT NULL,
-    CONSTRAINT "missileData_pkey" PRIMARY KEY (missile_id, "current_time", current_loc)
+    drop_rate real NOT NULL,
+    active boolean NOT NULL,
+    CONSTRAINT missile_data_pkey PRIMARY KEY (missile_id, "current_time", current_loc)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public."missileData"
+ALTER TABLE IF EXISTS public.missile_data
     OWNER to postgres;
